9a44a97371df0f9df5d1bd5219b513e1250050b9
Vollando <cvb.charles@hotmail.com>
2017-08-11 23:00:06 +0100

    basic movement implementation

--- Assets/ControllerScript.cs
+++ Assets/ControllerScript.cs
@@ -11,6 +11,26 @@ public class ControllerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        // Not using else if so any movements can link into another
+		if(Input.GetKey(GameManagerScript.GM.forward))
+        {
+            transform.position += Vector3.forward / 2;
+        }
+        if (Input.GetKey(GameManagerScript.GM.backward))
+        {
+            transform.position += Vector3.back / 2;
+        }
+        if (Input.GetKey(GameManagerScript.GM.left))
+        {
+            transform.position += Vector3.left / 2;
+        }
+        if (Input.GetKey(GameManagerScript.GM.right))
+        {
+            transform.position += Vector3.right / 2;
+        } // GetKeyDown on Jump prevents stacking
+        if (Input.GetKeyDown(GameManagerScript.GM.jump))
+        {
+            transform.position += Vector3.up / 2;
+        }
+    }
 }
