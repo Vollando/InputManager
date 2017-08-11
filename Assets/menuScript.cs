9e307fd0b8b47d83d116876fe1fb35d2f9ee2906
Vollando <cvb.charles@hotmail.com>
2017-08-11 23:54:37 +0100

    UI Updating buttons to reflect default keys

--- Assets/menuScript.cs
+++ Assets/menuScript.cs
@@ -1,16 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class menuScript : MonoBehaviour {
 
+    Transform menuPanel;
+    Event keyEvent;
+    Text buttonText;
+    KeyCode newKey;
+
+    bool waitingForKey;
+
 	// Use this for initialization
 	void Start () {
-		
+        menuPanel = transform.Find("Panel");
+        menuPanel.gameObject.SetActive(false);
+        waitingForKey = false;
+
+        // iterate through each child within the menu object
+        // updating the text the of child button object to equal the keyCode
+
+        for (int i = 0; i < menuPanel.childCount; i++)
+        {
+            if (menuPanel.GetChild(i).name == "forwardKey")
+                menuPanel.GetChild(i).GetComponentInChildren<Text>().text = GameManagerScript.GM.forward.ToString();
+            else if (menuPanel.GetChild(i).name == "backwardKey")
+                menuPanel.GetChild(i).GetComponentInChildren<Text>().text = GameManagerScript.GM.backward.ToString();
+            else if (menuPanel.GetChild(i).name == "leftKey")
+                menuPanel.GetChild(i).GetComponentInChildren<Text>().text = GameManagerScript.GM.left.ToString();
+            else if (menuPanel.GetChild(i).name == "rightKey")
+                menuPanel.GetChild(i).GetComponentInChildren<Text>().text = GameManagerScript.GM.right.ToString();
+            else if (menuPanel.GetChild(i).name == "jumpKey")
+                menuPanel.GetChild(i).GetComponentInChildren<Text>().text = GameManagerScript.GM.jump.ToString();
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKeyDown(KeyCode.Escape) && !menuPanel.gameObject.activeSelf) 
+            menuPanel.gameObject.SetActive(true);
+        else if (Input.GetKeyDown(KeyCode.Escape) && menuPanel.gameObject.activeSelf)
+            menuPanel.gameObject.SetActive(false);
+    }
 }
