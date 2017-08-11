ba83fbd378da1595935d092f937c9782892a0cdd
Vollando <cvb.charles@hotmail.com>
2017-08-11 22:37:08 +0100

    Created Scene, using Enum.Parse casts the preferred player input from the local machine to the keyCode

---
+++ Assets/GameManagerScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour {
+
+    public static GameManagerScript GM;
+
+    public KeyCode jump { get; set; }
+    public KeyCode forward { get; set; }
+    public KeyCode backward { get; set; }
+    public KeyCode left { get; set; }
+    public KeyCode right { get; set; }
+
+    void Awake()
+    {
+        // Creating a singleton 
+        if(GM == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            GM = this;
+        }
+        else if (GM != this)
+        {
+            Destroy(gameObject);
+        }
+
+        // Enum.Parse casts the preferred player input from the local machine to the keyCode
+        jump = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("jumpKey", "Space"));
+        forward = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("forwardKey", "W"));
+        backward = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("backwardKey", "S"));
+        left = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("leftKey", "A"));
+        right = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("rightKey", "D"));
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
